fdd5b86fbc1d8486b76f9fc54007cf797d5e8532
Kevin Egger <eggerk@ethz.ch>
2018-05-29 11:34:03 +0200

    Enable usage of linter from within submodules

--- linter.py
+++ linter.py
@@ -124,6 +124,20 @@ def check_cpp_lint(staged_files, cpplint_file, ascii_art, repo_root):
                                   "{}".format(changed_file))
 
       # Get relative path to repository root.
+      if os.path.isfile(os.path.join(repo_root, '.git')):
+        # Repo is a submodule, look for parent git repository containing this
+        # submodule.
+        search_path = repo_root
+        found_to_level_git_repo = False
+        for _ in range(10):
+          search_path = os.path.dirname(repo_root)
+          if os.path.isdir(os.path.join(search_path, '.git')):
+            found_to_level_git_repo = True
+            break
+
+        assert found_to_level_git_repo
+        repo_root = search_path
+
       common_prefix = os.path.commonprefix([
           os.path.abspath(repo_root), os.path.abspath(package_root)])
       package_root = os.path.relpath(package_root, common_prefix)
@@ -340,7 +354,7 @@ def get_whitelisted_files(repo_root, files, whitelist):
     for entry in whitelist:
       # Add trailing slash if its a directory and no slash is already there.
       if os.path.isdir(repo_root + '/' + entry):
-          entry = os.path.join(os.path.normpath(entry), '')
+        entry = os.path.join(os.path.normpath(entry), '')
 
       # Check if the file itself or its parent directory is in the whitelist.
       if (file == entry
@@ -361,10 +375,10 @@ def linter_check(repo_root, linter_subfolder):
   # Read linter config file.
   linter_config_file = repo_root + '/linterconfig.yaml'
   if os.path.isfile(repo_root + '/linterconfig.yaml'):
-      print("Found repo linter config: {}".format(linter_config_file))
-      linter_config = read_linter_config(linter_config_file)
+    print("Found repo linter config: {}".format(linter_config_file))
+    linter_config = read_linter_config(linter_config_file)
   else:
-      linter_config = DEFAULT_CONFIG
+    linter_config = DEFAULT_CONFIG
 
   print("Found linter subfolder: {}".format(linter_subfolder))
   print("Found ascii art file at: {}".format(ascii_art_file))
@@ -414,7 +428,7 @@ def linter_check(repo_root, linter_subfolder):
       cpp_lint_success = check_cpp_lint(
           whitelisted_files, cpplint_file, ascii_art, repo_root)
     else:
-        cpp_lint_success = True
+      cpp_lint_success = True
 
     # Use pylint to check for comimpliance with Tensofrflow python style guide.
     if linter_config['use_pylint']:
