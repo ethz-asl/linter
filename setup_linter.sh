cd91c540cc62a6e9bd3a58d92e501be6aa854e59
Kevin Egger <eggerk@ethz.ch>
2018-05-30 13:57:06 +0200

    Fix linter path for bash

--- setup_linter.sh
+++ setup_linter.sh
@@ -1,4 +1,10 @@
-LINTER_PATH="$( cd "$( dirname "$0" )" && pwd )"
+if [ ! -z $ZSH_NAME ] ; then
+  LINTER_PATH="$( cd "$( dirname "$0" )" && pwd )"
+elif [ ! -z $BASH ] ; then
+  LINTER_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+  echo "Linter: Unsupported shell! Only bash and zsh supported at the moment!"
+fi
 export LINTER_PATH
 
 export PATH="$PATH:$LINTER_PATH/bin"
